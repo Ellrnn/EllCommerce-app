a65fe46dedc1999987414c5e51d8e1c81e83a1db
Ellrnn <rannamead58@gmail.com>
2023-11-24 22:57:09 -0300

    Setup styled-components

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    white: "#ffffff",
+    black: "#000000",
+    blue: {
+      500: "#0F52BA",
+    },
+    gray: {
+      100: "#EEEEEE",
+      300: "#BFBFBF",
+      800: "#373737",
+      900: "#2C2C2C",
+    },
+  },
+};
