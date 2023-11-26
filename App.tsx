4e21d24053f42f53676c57aad2a4d2ba925bfaf7
Ellrnn <rannamead58@gmail.com>
2023-11-26 20:30:40 -0300

    Setup axios and react query

--- App.tsx
+++ App.tsx
@@ -8,10 +8,13 @@ import {
   Montserrat_700Bold,
 } from "@expo-google-fonts/montserrat";
 import * as SplashScreen from "expo-splash-screen";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { HomeScreen } from "./src/screens/Home/HomeScreen";
 import { theme } from "./src/global/styles/theme";
+
+const queryClient = new QueryClient();
 
 SplashScreen.preventAutoHideAsync();
 
@@ -34,10 +37,12 @@ export default function App() {
   }
 
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={theme}>
-        <HomeScreen />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={theme}>
+          <HomeScreen />
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
