c0943b6cf35976cf3dbc7abe013c1efb95ca8919
Ellrnn <rannamead58@gmail.com>
2023-11-27 22:26:09 -0300

    Create handleCartPress

--- src/screens/Home/HomeScreen.tsx
+++ src/screens/Home/HomeScreen.tsx
@@ -1,16 +1,24 @@
 import { StatusBar } from "expo-status-bar";
+import { DrawerScreenProps } from "@react-navigation/drawer";
 
 import S from "./styles";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
 import { Body } from "./components/Body/Body";
+import { AppDrawerParamList } from "../../routers/drawer/AppNavigator";
 
-export function HomeScreen() {
+type Props = DrawerScreenProps<AppDrawerParamList, "Home">;
+
+export function HomeScreen(props: Props) {
+  function handleCartPress() {
+    props.navigation.openDrawer();
+  }
+
   return (
     <>
       <StatusBar style="light" />
       <S.Container edges={["top"]}>
-        <Header />
+        <Header onCartButtonPress={handleCartPress} />
         <Body />
         <Footer />
       </S.Container>
--- src/screens/Home/components/Header/Header.tsx
+++ src/screens/Home/components/Header/Header.tsx
@@ -3,7 +3,11 @@ import S from "./styles";
 import { ShoppingCartIcon } from "../../../../../assets/icons/ShoppingCartIcon";
 import { useCartStore } from "../../../../stores/cartStore";
 
-export function Header() {
+type HeaderProps = {
+  onCartButtonPress: () => void;
+};
+
+export function Header(props: HeaderProps) {
   const { products } = useCartStore();
 
   return (
@@ -12,7 +16,7 @@ export function Header() {
         <S.Text type="primary">ELL</S.Text>
         <S.Text type="secondary">Commerce</S.Text>
       </S.TextWrapper>
-      <S.CartButton activeOpacity={0.7}>
+      <S.CartButton activeOpacity={0.7} onPress={props.onCartButtonPress}>
         <ShoppingCartIcon width={16} height={16} />
         <S.TextCart>{products.length}</S.TextCart>
       </S.CartButton>
