6492d24c857ff5602a948719bd2c56cf9801e777
Ellrnn <rannamead58@gmail.com>
2023-11-27 21:28:40 -0300

    Create addProduct function

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+
 import { Product } from "../services/productService";
 
 type CartProduct = Product & {
@@ -6,9 +7,26 @@ type CartProduct = Product & {
 };
 
 type CartState = {
+  /**Lista de produtos que estão no carrinho */
   products: CartProduct[];
+  /**Função que adiciona 1 produto ao carrinho */
+  addProduct: (product: Product) => void;
 };
 
-export const useCartStore = create<CartState>((set) => {
-  return { products: [] };
+export const useCartStore = create<CartState>((setCartState) => {
+  // Initial value of cart store
+  return {
+    products: [],
+
+    addProduct: (novoProduto) => {
+      setCartState((antigoEstadoDoCarrinho) => {
+        return {
+          products: [
+            ...antigoEstadoDoCarrinho.products,
+            { quantity: 1, ...novoProduto },
+          ],
+        };
+      });
+    },
+  };
 });
